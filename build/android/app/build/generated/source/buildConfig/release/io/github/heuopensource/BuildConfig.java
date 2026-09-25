25b83b950c4d76bd7e1cf9fd4d5081689de6fb48
only9464 <sky9464@qq.com>
2026-09-25 13:18:06 +0800

    初始化

---
+++ build/android/app/build/generated/source/buildConfig/release/io/github/heuopensource/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.github.heuopensource;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "io.github.heuopensource";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
